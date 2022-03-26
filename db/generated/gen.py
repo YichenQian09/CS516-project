71a9072697dfc7ceb7bd7479fff268509e01e881
xhu19980713 <huxinyu232@gmail.com>
2022-03-26 15:22:29 -0400

    generated toy dataset for user_browse and user_cart

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -65,31 +65,51 @@ def gen_user(num_users):
         print(f'{num_users} generated')
     return
 
-# generate fake dataset for users' citation cart
-# table User_cart
-
-# def gen_user_cart(num_users, num_papaers):
-#     user_has_cart = np.random_choice([True,False],num_users, p=[0.7,0.3])
-#     with open('db/generated/User_cart.csv',"w") as f:
-#         writer = get_csv_writer(f)
-#         print('User_cart...', end=' ', flush=True)
-#         for uid in range(num_users):
-
-
-
 # generate fake dataset for users' browsing history
 # table User_browse
+def gen_user_browse(num_users, num_papaers=629814):
+    user_browse_history =dict()
+    user_browse = np.random.choice([True,False],num_users, p=[0.95,0.05])
+    num_browsed = list(fake.random_int(min=10,max=40) for i in range(num_users))
+    with open('db/generated/User_browse.csv',"w") as f:
+        writer = get_csv_writer(f)
+        print('User_browse...', end=' ', flush=True)
+        for uid in range(num_users):
+            one_browse_history=dict()
+            if user_browse[uid]:
+                #min pid 0, max pid 629813
+                browsed_list = np.random.choice(num_papaers, size=num_browsed[uid])
+                for browsed_pid in browsed_list:
+                    time_browsed= fake.date_time()
+                    writer.writerow([uid,browsed_pid,time_browsed])
+                    one_browse_history.update({browsed_pid:time_browsed})
+            user_browse_history.update({uid:one_browse_history})
+        print(f'{num_users} generated')
+    return user_browse_history, user_browse, num_browsed
+
+# generate fake dataset for users' citation cart
+# table User_cart
+def gen_user_cart(num_users, browse_history, user_browse, num_browsed):
+    user_has_cart = np.random.choice([True,False],num_users, p=[0.7,0.3])
+    num_citation = list(fake.random_int(min=1,max=num_browsed[i]) for i in range(num_users))
+    with open('db/generated/User_cart.csv',"w") as f:
+        writer = get_csv_writer(f)
+        print('User_cart...', end=' ', flush=True)
+        for uid in range(num_users):
+            if user_has_cart[uid] and user_browse[uid]:
+                #min pid 0, max pid 629813
+                citation_list = np.random.choice(list(browse_history[uid].keys()), size=num_citation[uid])
+                for cite_pid in citation_list:
+                    time_cited = fake.date_time_between(start_date=browse_history[uid][cite_pid])
+                    writer.writerow([uid,cite_pid,time_cited])     
+        print(f'{num_users} generated')
+    return 
+
 
 # generate fake dataset for users' collection
 # table Collections 
 
-
-def gen_purchases(num_purchases,):
-
-            uid = fake.random_int(min=0, max=num_users-1)
-            pid = fake.random_element(elements=available_pids)
-            time_purchased = fake.date_time()
-            writer.writerow([id, uid, pid, time_purchased])
-
-gen_auth(num_users)
-gen_user(num_users)
+#gen_auth(num_users)
+#gen_user(num_users)
+user_browse_history, user_browse, num_browsed  =  gen_user_browse(num_users)
+gen_user_cart(num_users,user_browse_history, user_browse, num_browsed)
